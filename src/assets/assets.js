f6aec2d0c6835734683129211c87ee07b0e71dbe
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-01-20 21:29:09 +0530

    assets.js file updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,235 +1,280 @@
-import user_image from './user-image.png';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.jpeg';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import user_image from "./user-image.png";
+import code_icon from "./code-icon.png";
+import code_icon_dark from "./code-icon-dark.png";
+import edu_icon from "./edu-icon.png";
+import edu_icon_dark from "./edu-icon-dark.png";
+import project_icon from "./project-icon.png";
+import project_icon_dark from "./project-icon-dark.png";
+import vscode from "./vscode.png";
+import firebase from "./firebase.png";
+import figma from "./figma.png";
+import git from "./git.png";
+import mongodb from "./mongodb.png";
+import right_arrow_white from "./right-arrow-white.png";
+import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
+import mail_icon from "./mail_icon.png";
+import mail_icon_dark from "./mail_icon_dark.png";
+import profile_img from "./profile-img.jpeg";
+import download_icon from "./download-icon.png";
+import hand_icon from "./hand-icon.png";
+import header_bg_color from "./header-bg-color.png";
+import moon_icon from "./moon_icon.png";
+import sun_icon from "./sun_icon.png";
+import arrow_icon from "./arrow-icon.png";
+import arrow_icon_dark from "./arrow-icon-dark.png";
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import web_icon from "./web-icon.png";
+import mobile_icon from "./mobile-icon.png";
+import ui_icon from "./ui-icon.png";
+import graphics_icon from "./graphics-icon.png";
+import right_arrow from "./right-arrow.png";
+import send_icon from "./send-icon.png";
+import right_arrow_bold from "./right-arrow-bold.png";
+import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
 
 export const assets = {
-    user_image,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    mongodb,
-    right_arrow_white,
-    logo,
-    logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark,
-    
+  user_image,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  project_icon,
+  project_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  mongodb,
+  right_arrow_white,
+  logo,
+  logo_dark,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  download_icon,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  web_icon,
+  mobile_icon,
+  ui_icon,
+  graphics_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
 };
 
 export const workData = [
-    {
-        title: 'Bitebox',
-        description: 'I created this MERN stack-based food delivery app using React Bootstrap, Tailwind CSS, HTML, and CSS for the frontend. The app uses Context API for state management, integrates Twilio and Nodemailer for user authentication and notifications, and Stripe for payment processing. The admin page allows easy management of orders and food items.',
-        bgImage: '/work-13.png',
-        bgImage1: '/work-13(1).png',
-        bgImage2: '/work-13(2).png',
-    },
-    
-    {
-        title: 'STARBUCKS CLONE',
-        description: 'I created this site using Bootstrap 5, HTML, and CSS to deliver a responsive and visually appealing design with a focus on clean structure and modern styling.',
-        bgImage: '/work-7.png',
-        bgImage1: '/work-8.png',
-        url:'https://jithin7777.github.io/Starbucks_clone/',
+  {
+    title: "Bitebox",
+    description:
+      "I created this MERN stack-based food delivery app using React Bootstrap, Tailwind CSS, HTML, and CSS for the frontend. The app uses Context API for state management, integrates Twilio and Nodemailer for user authentication and notifications, and Stripe for payment processing. The admin page allows easy management of orders and food items.",
+    bgImage: "/work-13.png",
+    bgImage1: "/work-13(1).png",
+    bgImage2: "/work-13(2).png",
+  },
 
-    },
-    {
-        title: 'Customizable Dashboard',
-        description: 'I created this customizable dashboard using Next.js, Tailwind CSS, ShadCN UI, and Context API. The app allows users to manage widget visibility (show/hide) with a clean and modern design. It also integrates MongoDB for data storage, offering a flexible and dynamic experience.',
-        bgImage: '/work-14.png',
-        bgImage1: '/work-14(1).png',
-        url: 'https://customizable-dashboard-nextjs.vercel.app/',
-    },
-    
-    {
-        title: 'Google Gemini App',
-        description: 'I developed the Google Gemini App using React, React Bootstrap, and the Google Gemini API to create a sleek, responsive, and feature-rich application',
-        bgImage: '/work-5.png',
-        bgImage1: '/work-6.png',
-        url:'https://geminiclonesite.netlify.app/',
+  {
+    title: "STARBUCKS CLONE",
+    description:
+      "I created this site using Bootstrap 5, HTML, and CSS to deliver a responsive and visually appealing design with a focus on clean structure and modern styling.",
+    bgImage: "/work-7.png",
+    bgImage1: "/work-8.png",
+    bgImage2: "/work-7.png",
 
+    url: "https://jithin7777.github.io/Starbucks_clone/",
+  },
+  {
+    title: "Customizable Dashboard",
+    description:
+      "I created this customizable dashboard using Next.js, Tailwind CSS, ShadCN UI, and Context API. The app allows users to manage widget visibility (show/hide) with a clean and modern design. It also integrates MongoDB for data storage, offering a flexible and dynamic experience.",
+    bgImage: "/work-14.png",
+    bgImage1: "/work-14(1).png",
+    bgImage2: "/work-14.png",
 
-    },
-    {
-        title: 'Shopsphere',
-        description: 'I built Shopsphere, an e-commerce website, using React, Redux Toolkit, React Bootstrap, and Fake Store API to deliver a dynamic and responsive shopping experience.',
-        bgImage: '/work-10.png',
-        bgImage1: '/work-10(1).png',
-        url:'https://shopsphere1.netlify.app/',
+    url: "https://customizable-dashboard-nextjs.vercel.app/",
+  },
 
+  {
+    title: "Google Gemini App",
+    description:
+      "I developed the Google Gemini App using React, React Bootstrap, and the Google Gemini API to create a sleek, responsive, and feature-rich application",
+    bgImage: "/work-5.png",
+    bgImage1: "/work-6.png",
+    bgImage2: "/work-5.png",
 
-    },
-    {
-        title: 'Article sphere',
-        description: 'I developed Article Sphere, a blog application with full CRUD functionality, built using React, Firebase, and React Bootstrap, enabling seamless content creation, editing, and sharing.',
-        bgImage: '/work-11.png',
-        bgImage1: '/work-11(1).png',
-        url:'https://article-sphere-5.netlify.app/',
+    url: "https://geminiclonesite.netlify.app/",
+  },
+  {
+    title: "Shopsphere",
+    description:
+      "I built Shopsphere, an e-commerce website, using React, Redux Toolkit, React Bootstrap, and Fake Store API to deliver a dynamic and responsive shopping experience.",
+    bgImage: "/work-10.png",
+    bgImage1: "/work-10(1).png",
+    bgImage2: "/work-10.png",
 
+    url: "https://shopsphere1.netlify.app/",
+  },
+  {
+    title: "Article sphere",
+    description:
+      "I developed Article Sphere, a blog application with full CRUD functionality, built using React, Firebase, and React Bootstrap, enabling seamless content creation, editing, and sharing.",
+    bgImage: "/work-11.png",
+    bgImage1: "/work-11(1).png",
+    bgImage2: "/work-11.png",
 
-    },{
-        title: 'taste harmony',
-        description: 'I created Taste Harmony, a dynamic restaurant website using React, React Bootstrap, and a Restaurants API, offering interactive and responsive views for users.',
-        bgImage: '/work-9.png',
-        bgImage1: '/work-9(1).png',
-        url:'https://restauranttasteharmony.netlify.app/',
+    url: "https://article-sphere-5.netlify.app/",
+  },
+  {
+    title: "taste harmony",
+    description:
+      "I created Taste Harmony, a dynamic restaurant website using React, React Bootstrap, and a Restaurants API, offering interactive and responsive views for users.",
+    bgImage: "/work-9.png",
+    bgImage1: "/work-9(1).png",
+    bgImage2: "/work-9.png",
 
+    url: "https://restauranttasteharmony.netlify.app/",
+  },
+  {
+    title: "Contact hub",
+    description:
+      "A user management application built with React and React Bootstrap, integrated with a Users API to enable dynamic views ",
+    bgImage: "/works-12.png",
+    bgImage1: "/work-12(1).png",
+    bgImage2: "/works-12.png",
 
-    },
-    {
-        title: 'Contact hub',
-        description: 'A user management application built with React and React Bootstrap, integrated with a Users API to enable dynamic views ',
-        bgImage: '/works-12.png',
-        bgImage1: '/work-12(1).png',
-        url: 'https://contacthb.netlify.app/',
-    },
-    {
-        title: 'Frontend project',
-        description: 'Web Design',
-        bgImage: '/work-5.png', 
-    },
-   
-]
+    url: "https://contacthb.netlify.app/",
+  },
+];
 
 export const serviceData = [
-    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
-    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
-]
+  {
+    icon: assets.web_icon,
+    title: "Web design",
+    description: "Web development is the process of building, programming...",
+    link: "",
+  },
+  {
+    icon: assets.mobile_icon,
+    title: "Mobile app",
+    description:
+      "Mobile app development involves creating software for mobile devices...",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "UI/UX design",
+    description:
+      "UI/UX design focuses on creating a seamless user experience...",
+    link: "",
+  },
+  {
+    icon: assets.graphics_icon,
+    title: "Graphics design",
+    description: "Creative design solutions to enhance visual communication...",
+    link: "",
+  },
+];
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: "Languages",
+    description: "HTML, CSS, JavaScript React Js, Next Js",
+  },
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: "Education",
+    description: "B.Tech in Computer Science",
+  },
+  {
+    icon: assets.project_icon,
+    iconDark: assets.project_icon_dark,
+    title: "Projects",
+    description: "Built more than 5 projects",
+  },
 ];
 
 export const toolsData = [
-    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git
+  assets.vscode,
+  assets.firebase,
+  assets.mongodb,
+  assets.figma,
+  assets.git,
 ];
 
-
-
 export const skillsData = [
-    {
-        name: 'HTML',
-        logo: '/html-1.svg', // HTML logo
-    },
-    {
-        name: 'CSS',
-        logo: '/css-3.svg', // CSS logo
-    },
-    {
-        name: 'JavaScript',
-        logo: '/javascript-1.svg', // JavaScript logo
-    },
-    {
-        name: 'Bootstrap',
-        logo: '/bootstrap-5-1.svg', // Bootstrap logo
-    },
-    {
-        name: 'Tailwind CSS',
-        logo: '/tailwindcss.svg', // Tailwind CSS logo
-    },
-    {
-        name: 'React',
-        logo: '/react-2.svg', // React logo
-    },
-    {
-        name: 'Next.js',
-        logo: '/next.svg', // Next.js logo
-    },
-    {
-        name: 'Redux',
-        logo: '/redux.svg', // Redux logo
-    },
-    {
-        name: 'Angular',
-        logo: '/angular-icon.svg', // Angular logo
-    },
-    {
-        name: 'Node.js',
-        logo: '/nodejs-2.svg', // Node.js logo
-    },
-    {
-        name: 'MongoDB',
-        logo: '/mongodb-icon-1.svg', // MongoDB logo
-    },
-    {
-        name: 'PostgreSQL',
-        logo: '/postgresql.svg', // PostgreSQL logo
-    },
-    {
-        name: 'GitHub',
-        logo: '/github-icon-1.svg', // GitHub logo
-    },
-    {
-        name: 'Docker',
-        logo: '/docker-4.svg', // Docker logo
-    },
-    {
-        name: 'Vercel',
-        logo: '/vercel.svg', // Vercel logo
-    },
+  {
+    name: "HTML",
+    logo: "/html-1.svg", 
+  },
+  {
+    name: "CSS",
+    logo: "/css-3.svg", 
+  },
+  {
+    name: "JavaScript",
+    logo: "/javascript-1.svg", 
+  },
+  {
+    name: "Bootstrap",
+    logo: "/bootstrap-5-1.svg", },
+  {
+    name: "Tailwind CSS",
+    logo: "/tailwindcss.svg", 
+  },
+  {
+    name: "React",
+    logo: "/react-2.svg", 
+  },
+  {
+    name: "Next.js",
+    logo: "/next.svg", 
+  },
+  {
+    name: "Redux",
+    logo: "/redux.svg", 
+  },
+  {
+    name: "Angular",
+    logo: "/angular-icon.svg", 
+  },
+  {
+    name: "Node.js",
+    logo: "/nodejs-2.svg",
+  },
+  {
+    name: "MongoDB",
+    logo: "/mongodb-icon-1.svg", 
+  },
+  {
+    name: "PostgreSQL",
+    logo: "/postgresql.svg", 
+  },
+  {
+    name: "GitHub",
+    logo: "/github-icon-1.svg", 
+  },
+  {
+    name: "Docker",
+    logo: "/docker-4.svg", 
+  },
+  {
+    name: "Vercel",
+    logo: "/vercel.svg", 
+  },
 ];
