7fcf3496a16459ef7e8ae5556061fc6f8a19e62a
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-01-26 20:04:19 +0530

    navbar updated

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import ThemeToggle from "./partials/ThemeToggle";
 import ContactButton from "./partials/ContactButton";
 import MenuToggle from "./partials/MenuToggle";
-import Logo from "./partials/Logo";
 import MobileMenu from "../MobileMenu/MobileMenu";
 
- const Navbar = () => {
+const Navbar = () => {
   const menuRef = useRef();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(true); 
+  const [prevScrollY, setPrevScrollY] = useState(0); 
+  const visibilityThreshold = 200; // Height in pixels before hiding logic starts
 
   const openMenu = () => {
     setIsMenuOpen(true);
@@ -20,11 +22,40 @@ import MobileMenu from "../MobileMenu/MobileMenu";
     menuRef.current.style.transform = "translateX(-100%)";
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > visibilityThreshold) {
+       
+        if (currentScrollY > prevScrollY) {
+          setIsVisible(false);
+        } else {
+          setIsVisible(true);
+        }
+      } else {
+        
+        setIsVisible(true);
+      }
+
+      setPrevScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollY]);
+
   return (
     <div>
-      <nav className="fixed w-full lg:px-8 xl:px-[8%] px-5 flex items-center justify-between z-50 mt-3 bg-beige shadow-lg">
-      <h1 className="font-Ovo mb-3  text-2xl"> Jithin Jose</h1>
-        <ul className="hidden md:flex space-x-8 rounded-md py-3 px-12 mb-3 bg-white shadow-md">
+      <nav
+        className={`fixed w-full lg:px-8 xl:px-[8%] px-5 flex items-center justify-between z-50 bg-beige shadow-lg transition-transform duration-300 ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <h1 className="font-Ovo mb-3 text-2xl mt-3">Jithin Jose</h1>
+        <ul className="hidden md:flex space-x-8 rounded-md py-3 px-12 mb-3 mt-3 bg-white shadow-md">
           <li>
             <a
               href="#top"
@@ -33,15 +64,6 @@ import MobileMenu from "../MobileMenu/MobileMenu";
               Home
             </a>
           </li>
-          {/* <li>
-            <a
-              href="#about"
-              className="text-charcoal hover:text-teal transition duration-300 font-Ovo"
-            >
-              About Me
-            </a>
-          </li> */}
-          
           <li>
             <a
               href="#work"
