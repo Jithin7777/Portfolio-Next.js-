f77c14341bb0b3cd5d4f44872106770e4f98ec3e
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-12-09 14:24:05 +0530

    updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,18 +10,18 @@ const Skills = () => {
         Technologies I have experience with.
       </p>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 px-5 sm:px-10 lg:px-24 mt-10">
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 px-5 sm:px-10 lg:px-24 mt-10">
         {skillsData.map((skill, index) => (
           <div
             key={index}
-            className="flex flex-col items-center text-center p-6 bg-white rounded-2xl shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl backdrop-blur-lg"
+            className="flex flex-col items-center text-center p-3 md:p-6 bg-white rounded-2xl shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl backdrop-blur-lg"
           >
             <img
               src={skill.logo || "/fallback-image.png"}
               alt={`${skill.name} Logo`}
-              className="w-16 h-16 sm:w-20 sm:h-20 transition-transform duration-300 hover:scale-110"
+              className=" h-12 md:w-16 md:h-16  transition-transform duration-300 hover:scale-110"
             />
-            <p className="text-lg sm:text-xl font-medium text-gray-800 mt-3">
+            <p className="md:text-lg text-sm font-medium text-gray-800 mt-3">
               {skill.name}
             </p>
             <p className="text-sm sm:text-base text-gray-500 mt-1">
